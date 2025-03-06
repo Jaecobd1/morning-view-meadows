06f5dd0190471714f95d01d44e01acc31f5186b8
Jake Dobler <jaecob.dobler@reusser.com>
2025-03-05 19:29:38 -0500

    removing bad header

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -31,10 +31,6 @@ export default function GalleryComponent() {
 
   return (
     <div className=" mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">
-        Pinterest-Style Gallery
-      </h1>
-
       {/* Masonry Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
         {galleryItems.map((item) => (
